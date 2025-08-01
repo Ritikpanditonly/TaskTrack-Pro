33c3184c195753f82d4b19528d732ea6e8b2e0b7
Ritikpanditonly <pandeyritik236@gmail.com>
2025-08-01 13:28:09 +0530

    update

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const app = express();
 
+
 dotenv.config();
 connectDB();
 
@@ -22,11 +23,9 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes); // <-- VERY IMPORTANT
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+//app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
